89ae80dc55dd3952d6daa590ae23c7784ea3c651
kazukii4 <boika.asanbekov@gmail.com>
2020-06-23 13:30:29 +0600

    mil

--- js/index.js
+++ js/index.js
@@ -1,37 +1,80 @@
-var name =prompt("введите ваше имя")
+var questions = ["столица Кыргызстана? ", "Кто изображен на 100$? ", "сколько пальцев на руке? ", "Столица Грузии? ", "Столица России? "]
+var answers = ["Шанхай, Душанбе, Баткен, Бишкек", "Адам, Бенжамин, супермен, Сооронбай", "5, 10, 250, 1", "Шанхай, Тбилиси, Баткен, Бишкек", "Шанхай, Тбилиси, Москва, Бишкек"]
+var quesAns = ["Бишкек", "Бенжамин", 5, "Тбилиси", "Москва"]
 
-var q1 =prompt(name + "кто быстрее?: 1-лошадь 2-собака 3-кошка 4-черепаха")
-    if (q1==1){
-        alert("next")
-        var q2 = prompt(name + "какая марка делает лучшие машины?: 1-ролс 2-бентли 3-ваг 4-lx")
-    if (q2==1) {
-        alert("отлично идём дальше")
-        var q3 = prompt("самая узнаваемая компания: 1-xiomi 2-huawei 3-apple 4-samsung")
-        if (q3==3) {
-            alert("exelent work")
-            var q4 =prompt("какой язык для програмиррования легче других из перечисленных: 1-c++ 2-js 3-java 4-c#")
-            if (q4==2) {
-                alert("Л-логично")
-                var q5 =prompt(name+ "последний вопрос, какая страна влияет на весь мир: 1-Kg 2-ru 3-uk 4-USA")
-                if (q5==4) {
-                    alert("поздравляем вы выиграли")
-                }
-            }
-            else{
-                alert(кет)
-            }
+var sumsOfQestions = [] // пустой список для подсчета сумм
+
+for (var a = 1; a<questions.length+1; a++){
+    sumsOfQestions.push(500*a)
+} // цикл считающий суммы 
+
+var name = prompt("Введите ваше имя")
+
+var sum = prompt("Введите несгораемую сумму "+ sumsOfQestions)
+
+var count, allSum, i , otvet // перечисление переменных 
+count=0 //количество правельных ответов
+allSum=0 //общая сумма выйигрыша
+
+for(i=0; i<questions.length; i++){  // цикл переберающий вопросы
+    otvet= prompt("уважаемый "+name+ " ответьте на следующий вопрос "+ questions[i] + answers[i])
+    if(otvet == quesAns[i]){
+        alert("Cool")
+        count++
+        allSum = allSum+500
+    }else{
+        alert("Извините но вы проиграли")
+        alert("вы ответили на "+ count +" вопроса из "+ questions.length)
+        if(allSum>=sum){ //проверка на то что наша несгороемая сумма соответсвует выйигрышу
+            alert("ваш выйигрыш = " + sum)
+        }else{
+            alert("ваш выйигрыш = 0")
         }
-        else{
-            alert(кет)
-        }
+       
+        break
     }
-    else{
-        alert(кет)
-    }
-    }
-    else{
-        alert(кет)
-    }
+}
+
+alert("Поздравляю "+name+"Вы выйграли максимальную сумму денег в размере "+ allSum)
+
+
+
+
+// var arr = []
+
+// for (var i=0; i<=100; i++){
+
+//     if (i%3 == 0){
+//         arr.push()
+//         console.log(arr + "true")
+//     }else{
+//         arr.push()
+//         console.log(arr + "false")
+//     }
+// }
+// console.log(arr)
+
+
+
+
+// var arr=['qwerty', 123, 2345.65, "asdd342", true]
+
+// console.log(arr)
+
+// arr.push("12324234")
+
+// console.log(arr)
+// arr.pop()
+
+// console.log(arr)
+
+// for(var i=0; i<arr.length; i++){
+//    console.log(arr[i])
+//    if(arr[i] == 123){
+//        console.log(arr[i] + "имеется в вашем списке")
+//        break
+//    }
+// } цикл
     
 
 
